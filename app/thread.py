d62be2c94cebb0d004a4afc6602b10bf5fc98aba
kaka <vn503024@gmail.com>
2018-01-11 17:02:23 +0800

    Maintain: 完成mnist 手寫辨識雛形

--- app/thread.py
+++ app/thread.py
@@ -1,41 +1,31 @@
-import numpy as np
 import cv2
+import numpy as np
+from keras.models import Sequential, load_model
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 class VideoThread(QtCore.QThread):
     """ This thread is capture video with opencv """
-    image_data = QtCore.pyqtSignal(QtGui.QImage)
+    image_data = QtCore.pyqtSignal(np.ndarray)
     done_sig = QtCore.pyqtSignal()
 
-    def __init__(self, camera_port=0, parent=None):
+    def __init__(self, model, camera_port=0, parent=None):
         super(VideoThread, self).__init__(parent)
 
+        self.model = model
         self.camera_port = camera_port
         self.running = False
 
     @QtCore.pyqtSlot()
     def startVideo(self):
         self.camera = cv2.VideoCapture(self.camera_port)
-        #self.camera.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-        #self.camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
-        #self.camera.set(cv2.CAP_PROP_FPS, 30)
         self.running = True
         
         while self.running:
-            ret, frame = self.camera.read() # type: np.ndarray
+            ret, frame = self.camera.read()
 
             if ret:
-                # OpenCV stores data in BGR format. 
-                # Qt stores data in RGB format.
-                qt_image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) # type: np.ndarray
-                #qt_image_2 = cv2.resize(qt_image, (640, 480), interpolation=cv2.INTER_AREA)
-                height, width = qt_image.shape[:2]
-                bytesPerLine = 3 * width # qt_image.strides[0] = 3 * width
-
-                # QImage.Format_RGB888: 圖使用 8-8-8 24位RGB格式
-                image = QtGui.QImage(qt_image.data, width, height, bytesPerLine, QtGui.QImage.Format_RGB888)
-
-                self.image_data.emit(image)
+                self.test(frame)
+                self.image_data.emit(frame)
         
         self.camera.release()
         cv2.destroyAllWindows()
@@ -43,3 +33,23 @@ class VideoThread(QtCore.QThread):
     
     def stopVideo(self):
         self.running = False
+    
+    def test(self, frame):
+        gray_image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        ret, thresh1 = cv2.threshold(gray_image, 127, 255, cv2.THRESH_BINARY_INV)
+        
+        image = cv2.resize(thresh1, (28, 28), interpolation=cv2.INTER_AREA)
+        cv2.imwrite('x_test.png', image)
+        image = image.reshape(1, 28*28)
+        image = image.astype('float32')
+        image = image / 255
+
+        print('=========================')
+        x_test = image.reshape(1, 28, 28, 1)
+
+        scores = self.model.predict(x_test)
+        top_label_ix = np.argmax(scores)
+        print("Predict Label: {0}".format(top_label_ix))
+        print('=========================')
+
+       
--- gui/videobox.py
+++ gui/videobox.py
@@ -1,6 +1,11 @@
+import sys
+import cv2
+import numpy as np
 from PyQt5 import QtCore, QtGui, QtWidgets
 from gui.ui_videobox import Ui_VideoBox
 from app.thread import VideoThread
+
+from keras.models import Sequential, load_model
 
 class ImageViewer(QtWidgets.QWidget):
     def __init__(self, parent = None):
@@ -17,11 +22,9 @@ class ImageViewer(QtWidgets.QWidget):
         painter.drawImage(0, 0, self.image)
         self.image = QtGui.QImage()
         
-    @QtCore.pyqtSlot(QtGui.QImage)
-    def setImage(self, image):
-        if image.isNull():
-            print("Viewer Dropped frame!")
-
+    @QtCore.pyqtSlot(np.ndarray)
+    def setImage(self, frame):
+        image = self.get_qt_image(frame)
         self.image = image
         
         if image.size() != self.size():
@@ -30,15 +33,32 @@ class ImageViewer(QtWidgets.QWidget):
         # QWidgwe.update(self): 
         # calling update() several times normally results in just one paintEvent() call.
         self.update()
+    
+    def get_qt_image(self, frame):
+        # OpenCV stores data in BGR format. 
+        # Qt stores data in RGB format.
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        image = cv2.resize(image, (28, 28), interpolation=cv2.INTER_AREA)
+
+        height, width = image.shape[:2]
+        bytesPerLine = 3 * width # image.strides[0] = 3 * width
+
+        qt_image = QtGui.QImage(image.data, width, height, bytesPerLine, QtGui.QImage.Format_RGB888)
+
+        return qt_image
 
 class VideoBox(QtWidgets.QGroupBox):
     def __init__(self, parent=None):
         super(VideoBox, self).__init__(parent)
 
         self.image_viewer = ImageViewer()
-        #self.video_worker = VideoThread()
-    
         self.__threads = None
+
+        path = sys.path[0]
+        self.model = load_model(path + '/cnn_demo_model.h5')
+        # 初始化加載模型後，需随便生成一个向量讓model先執行一次predict
+        # 之後使用才不會出現 ValueError
+        self.model.predict(np.zeros((1, 28, 28, 1)))
 
         self.ui = Ui_VideoBox()
         self.ui.setupUi(self)
@@ -51,7 +71,7 @@ class VideoBox(QtWidgets.QGroupBox):
     def start(self):
         self.__threads = []
         thread = QtCore.QThread(self)
-        video_worker = VideoThread()
+        video_worker = VideoThread(self.model)
         self.__threads.append((thread, video_worker))
         video_worker.moveToThread(thread)
 
@@ -60,6 +80,7 @@ class VideoBox(QtWidgets.QGroupBox):
         
         thread.started.connect(video_worker.startVideo)
         thread.start()
+
     
     def stop(self):
         for thread, worker in self.__threads:
